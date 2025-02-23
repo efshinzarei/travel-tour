c051c9d997e9bbbe33fba4fa4104dc05eaf7c2e4
Afshin Zarei <afshin.zarei2000@gmail.com>
2025-02-23 16:10:14 +0330

    Added mobile menu.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,16 @@
 const navBtn = document.querySelector(".nav__btn");
+const navMenu = document.querySelector(".nav-menu");
 
 let navOpen = false;
 
 navBtn.addEventListener("click", () => {
   if (navOpen) {
     navBtn.classList.remove("nav__btn--open");
+    navMenu.classList.remove("nav-menu--open");
     navOpen = false;
   } else {
     navBtn.classList.add("nav__btn--open");
-    navOpen = true; 
+    navMenu.classList.add("nav-menu--open");
+    navOpen = true;
   }
 });
